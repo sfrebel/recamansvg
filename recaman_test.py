51f05054da94b49c9cda95a09ca54956ff98d850
Sebastian Frebel <sebastian.frebel@t-online.de>
2020-04-26 11:04:19 +0200

    added more tests and travis script

--- recaman_test.py
+++ recaman_test.py
@@ -5,3 +5,12 @@ def test_uniqeness():
     sequence = calculate_racaman(200)
     for elem in sequence:
         assert sequence.count(elem) == 1
+    sequence = calculate_racaman(400)
+    for elem in sequence:
+        assert sequence.count(elem) == 1
+
+def test_sequencelength():
+    sequence = calculate_racaman(200)
+    assert len(sequence) == 200
+    sequence = calculate_racaman(42)
+    assert len(sequence) == 42
